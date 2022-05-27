a73234e04d9d62f00b0db041f42c3998514b9bbd
Жаков Максим Вадимович <max.zhakov@gmail.com>
2022-05-27 17:50:44 +0500

    Сделать объект, с полями фильтрации, сделать спецификацию(?), сделать через Criteria, обработать все в сервисном слое, контроллер через RequestParam

--- src/main/java/com/example/storage/repository/FileRepository.java
+++ src/main/java/com/example/storage/repository/FileRepository.java
@@ -4,6 +4,10 @@ import com.example.storage.model.FileModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface FileRepository extends JpaRepository<FileModel, String>, CustomFileRepository {
+    List<FileModel> findByDateBefore(LocalDateTime dateTime);
 }
